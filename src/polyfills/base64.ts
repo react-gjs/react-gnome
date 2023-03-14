76bf70d50e0ecaa88bb8b59b869cf1e7598c28af
ncpa0cpl <sbretner@touchbistro.com>
2023-03-14 13:05:18 +0100

    fix: btoa text encoding

--- src/polyfills/base64.ts
+++ src/polyfills/base64.ts
@@ -6,9 +6,6 @@ export function atob(data: string) {
 }
 
 export function btoa(data: string) {
-  const buffer = new Uint8Array(data.length);
-  for (let i = 0; i < data.length; i++) {
-    buffer[i] = data.charCodeAt(i);
-  }
-  return GLib.base64_encode(buffer, buffer.byteLength);
+  const encodedData = new TextEncoder().encode(data);
+  return GLib.base64_encode(encodedData)!;
 }
