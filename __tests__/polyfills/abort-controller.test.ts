d8699d611b69d2d45f5318aad7b8c6f4650314d0
ncpa0cpl <32438334+ncpa0cpl@users.noreply.github.com>
2024-05-14 10:13:24 +0200

    fix: abort controller impl fixed

--- __tests__/polyfills/abort-controller.test.ts
+++ __tests__/polyfills/abort-controller.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it, match } from "@reactgjs/gest";
 import "../../src/polyfills/abort-controller";
+import { waitUntil } from "../utils/wait-until";
 
 export default describe("AbortController", () => {
   it("should be defined", () => {
@@ -95,7 +96,106 @@ export default describe("AbortController", () => {
       controller.abort();
 
       expect(controller.signal.reason).toMatch(match.instanceOf(Error));
-      expect(controller.signal.reason.message).toBe("Signal was aborted.");
+      expect(controller.signal.reason.message).toBe("signal is aborted without reason");
+    });
+
+    it("should always throw the given reason", () => {
+      const controller = new AbortController();
+
+      controller.abort("Custom reason");
+
+      expect(() => controller.signal.throwIfAborted()).toThrow("Custom reason");
+    });
+
+    it("emitted event should contain all required properties", () => {
+      const controller = new AbortController();
+
+      let event: Event | null = null;
+      controller.signal.addEventListener("abort" as any, (ev) => {
+        event = ev;
+      });
+
+      controller.abort();
+
+      expect(event).toBeDefined();
+      expect(event!.target).toBe(controller.signal);
+      expect(event!.currentTarget).toBe(controller.signal);
+      expect(event!.timeStamp).toEqual(0);
+    });
+  });
+
+  describe("AbortSignal static", () => {
+    it("abort() should return an aborted signal", () => {
+      const signal = AbortSignal.abort();
+      expect(signal.aborted).toBe(true);
+      expect(signal.reason).toMatch(match.instanceOf(Error));
+
+      const signal2 = AbortSignal.abort("Custom reason");
+      expect(signal2.aborted).toBe(true);
+      expect(signal2.reason).toBe("Custom reason");
+    });
+
+    it("timeout() should return a signal that will be aborted after the given time", async () => {
+      const signal = AbortSignal.timeout(100);
+
+      expect(signal.aborted).toBe(false);
+
+      let event: Event | null = null;
+      signal.addEventListener("abort" as any, (ev) => {
+        event = ev;
+      });
+
+      await waitUntil(() => signal.aborted, 150);
+
+      expect(signal.aborted).toBe(true);
+      expect(signal.reason).toMatch(
+        match.allOf(
+          match.instanceOf(Error),
+          { message: "signal timed out" },
+        ),
+      );
+      expect(event).toBeDefined();
+      expect(event!.target).toBe(signal);
+      expect(event!.currentTarget).toBe(signal);
+      expect(event!.timeStamp >= 100).toEqual(true);
+    });
+
+    it("any() should return a signal that will be aborted when any of the given signals is aborted", () => {
+      const controller1 = new AbortController();
+      const controller2 = new AbortController();
+      const controller3 = new AbortController();
+
+      // @ts-expect-error
+      const signal: AbortSignal = AbortSignal.any([
+        controller1.signal,
+        controller2.signal,
+        controller3.signal,
+      ]);
+
+      expect(signal.aborted).toBe(false);
+
+      let event: Event | null = null;
+      let callCount = 0;
+      signal.addEventListener("abort", (ev) => {
+        event = ev;
+        callCount++;
+      });
+
+      controller2.abort({ message: "Custom reason" });
+
+      expect(callCount).toBe(1);
+      expect(signal.aborted).toBe(true);
+      expect(signal.reason).toMatch(
+        { message: "Custom reason" },
+      );
+      expect(event).toBeDefined();
+      expect(event!.target).toBe(signal);
+      expect(event!.currentTarget).toBe(signal);
+
+      controller1.abort();
+      controller3.abort();
+
+      expect(callCount).toBe(1);
     });
   });
 });
--- src/polyfills/abort-controller.ts
+++ src/polyfills/abort-controller.ts
@@ -4,17 +4,32 @@ registerPolyfills(
   "AbortController",
   "AbortSignal",
   "AbortError",
+  "TimeoutError"
 )(() => {
   enum Events {
     Abort = "abort",
   }
 
   class Event {
+    target: any;
+    currentTarget: any;
+    timeStamp: number = 0;
+
+    bubbles = false;
+    cancelable = false;
+    cancelBubble = false;
+    composed = false;
+    defaultPrevented = false;
+    eventPhase = 0;
+    isTrusted = true;
+    returnValue = true;
+
     constructor(public type: Events) {}
   }
 
   class EventController<E = Events> {
-    private listeners: Map<E, Array<(ev: Event) => void>> = new Map();
+    private listeners: Map<E, Array<(this: any, ev: Event) => void>> =
+      new Map();
 
     constructor() {}
 
@@ -42,7 +57,7 @@ registerPolyfills(
       }
       this.listeners.get(event)!.forEach(async (listener) => {
         try {
-          await listener(ev);
+          await listener.call(ev.target, ev);
         } catch (e) {
           console.error(e);
         }
@@ -55,16 +70,26 @@ registerPolyfills(
   }
 
   class AbortError extends Error {
-    constructor(message: string) {
-      super(message);
+    code = 20;
+    constructor(msg?: string) {
+      super(msg ?? "signal is aborted without reason");
       this.name = "AbortError";
+    }
+  }
+
+  class TimeoutError extends Error {
+    code = 23;
+    constructor() {
+      super("signal timed out");
+      this.name = "TimeoutError";
     }
   }
 
   class AbortSignal {
     static abort(reason?: any) {
       const signal = new AbortSignal();
-      signal._abort(reason ?? new AbortError("Signal was aborted."));
+      signal._isAborted = true;
+      signal._reason = reason ?? new AbortError();
       return signal;
     }
 
@@ -72,8 +97,20 @@ registerPolyfills(
       const signal = new AbortSignal();
 
       setTimeout(() => {
-        if (!signal.aborted) signal._abort(new AbortError("Timeout"));
+        if (!signal.aborted) signal._abort(new TimeoutError());
       }, time);
+
+      return signal;
+    }
+
+    static any(signals: Iterable<AbortSignal>) {
+      const signal = new AbortSignal();
+
+      for (const s of signals) {
+        s.addEventListener(Events.Abort, function (this: AbortSignal) {
+          if (!signal.aborted) signal._abort(this.reason);
+        });
+      }
 
       return signal;
     }
@@ -81,6 +118,7 @@ registerPolyfills(
     private readonly _events = new EventController();
     private _isAborted = false;
     private _reason: any = null;
+    private _startTime = Date.now();
 
     public onabort: ((ev: Event) => void) | null = null;
 
@@ -93,10 +131,15 @@ registerPolyfills(
     }
 
     constructor() {
-      this._events.add(
-        Events.Abort,
-        () => this.onabort && this.onabort(new Event(Events.Abort)),
-      );
+      this._events.add(Events.Abort, (ev) => this.onabort && this.onabort(ev));
+    }
+
+    protected _abortEvent() {
+      const e = new Event(Events.Abort);
+      e.currentTarget = this;
+      e.target = this;
+      e.timeStamp = Math.round(Date.now() - this._startTime);
+      return e;
     }
 
     protected _abort(reason?: any) {
@@ -104,8 +147,8 @@ registerPolyfills(
         return;
       }
       this._isAborted = true;
-      this._reason = reason ?? new AbortError("Signal was aborted.");
-      this._events.emit(Events.Abort, new Event(Events.Abort));
+      this._reason = reason ?? new AbortError();
+      this._events.emit(Events.Abort, this._abortEvent());
       this._events.clear();
     }
 
@@ -131,8 +174,8 @@ registerPolyfills(
       return this._signal;
     }
 
-    abort() {
-      this._signal["_abort"]();
+    abort(reason?: any) {
+      this._signal["_abort"](reason);
     }
   }
 
@@ -140,5 +183,6 @@ registerPolyfills(
     AbortController,
     AbortSignal,
     AbortError,
+    TimeoutError,
   };
 });
